3e721aa2fc34b6cb2e597346aba59e33ecbce8f1
User <glebovolod@gmail.com>
2026-04-22 18:19:00 +0300

    67

--- data/liked.py
+++ data/liked.py
@@ -5,6 +5,8 @@ from .db_session import SqlAlchemyBase
 class Liked(SqlAlchemyBase):
     __tablename__ = 'liked'
 
-    login = sqlalchemy.Column(sqlalchemy.String,
-                           primary_key=True)
-    games = sqlalchemy.Column(sqlalchemy.String)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'), nullable=False)
+    route_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    checkpoint_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=sqlalchemy.func.now())
